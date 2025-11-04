93e7b032527aebda219d99293286d4c49ed515eb
maulidarraihan <148992010+maulidarraihan@users.noreply.github.com>
2025-11-04 23:43:20 +0700

    Commit No. 1 – add first & second worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.lab_week_08
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Toast
+import androidx.work.*
+
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
+
+    private val workManager by lazy {
+        WorkManager.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
+            .build()
+
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("First process is done")
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("Second process is done")
+                }
+            }
+    }
+
+    private fun getIdInputData(idKey: String, idValue: String) =
+        Data.Builder()
+            .putString(idKey, idValue)
+            .build()
+
+    private fun showResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
